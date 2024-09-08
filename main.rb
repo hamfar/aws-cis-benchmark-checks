c8463d3cf5fd6e9162a4aeaeb9675ada966553ff
Hamza Fareed <h.fareed@gmail.com>
2024-09-08 20:56:54 +0200

    allow running multiple aws profiles check

--- main.rb
+++ main.rb
@@ -2,8 +2,21 @@ require 'aws-sdk'
 require_relative 'services/s3_checks'
 
 
-s3_client = Aws::S3::Client.new
-s3_checks = S3Checks.new(s3_client)
+def create_client(profile)
+    Aws::S3::Client.new(profile: profile)
+end
 
+def run_checks_for_profile(profile)
+    s3_client = create_client(profile)
+    s3_checks = S3Checks.new(s3_client)
+    s3_checks.run_checks
+end
 
-puts s3_checks.run_checks 
+    
+profiles = ['default']
+
+results = profiles.flat_map do |profile|
+    run_checks_for_profile(profile)
+end
+
+puts results.to_json
